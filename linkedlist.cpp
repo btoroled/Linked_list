4c9bcd025fd30036950fd9b501c431d6506c72d6
elbull-tarhur <benja.toro.l98@gmail.com>
2026-04-07 22:17:48 -0500

    Creacion de la clase inicial y de 4 metodos

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -5,34 +5,64 @@
 #include "linkedlist.h"
 #include <iostream>
 
-
 using namespace std;
 
 template<typename T>
 class LinkedList {
-    private:
+private:
     struct Node {
         T value;
-        Node *next;
+        Node* next;
 
-        Node(const T& val): value(val), next(nullptr) {}
+        Node(const T& val) : value(val), next(nullptr) {}
     };
+
     Node* head;
-    Node* tail;
 
 public:
-    LinkedList(): head(nullptr), tail(nullptr){}
+    LinkedList() : head(nullptr) {}
+
+    ~LinkedList() {
+        clear();
+    }
+
+    T front(Node* head) {
+        return head->value;
+    }
+
+    T back(Node* head) {
+        Node* prev = head;
+        while (head != nullptr) {
+            prev = head;
+            head = head->next;
+        }
+        return head->value;
+    }
+
+    void push_front(Node* insert, Node* head) {
+        insert->next = head;
+    }
+
+    void push_back(Node* insert, Node* head) {
+        while (head->next != nullptr) {
+            head = head->next;
+        }
+        head->next = insert;
+    }
 
     bool empty() {
-        if (head!= nullptr && head->next != nullptr)
-            return true;
+        return head == nullptr;
     }
 
     int size() {
-        int count =0;
-        while (head != nullptr) {
+        int count = 0;
+        Node* current = head;
+
+        while (current != nullptr) {
             count++;
+            current = current->next;
         }
+
         return count;
     }
 
@@ -45,38 +75,47 @@ public:
         }
     }
 
-    Node reverse(Node* head) {
-        Node* temp1;
-        Node* temp2;
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* nextNode = nullptr;
 
-        if (head != nullptr) {
-            temp1 = head->next;
-            head->next = nullptr;
-            while (temp1 != nullptr) {
-                temp2 = temp1->next;
-                temp1->next = head;
-                head = temp1;
-                temp1 = temp2;
-            }
+        while (current != nullptr) {
+            nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
         }
-        return head;
+
+        head = prev;
     }
 
-    void pop_front(Node* head) {
-        Node* temp;
-        temp = head->next;
-        delete head;
-        head = temp;
+    void pop_front() {
+        if (head == nullptr) return;
+
+        Node* temp = head;
+        head = head->next;
+        delete temp;
     }
 
-    void pop_back(Node* head) {
-        while (head->next != nullptr) {
-            head = head->next;
+    void pop_back() {
+        if (head == nullptr) return;
+
+        if (head->next == nullptr) {
             delete head;
+            head = nullptr;
+            return;
         }
+
+        Node* prev = nullptr;
+        Node* current = head;
+
+        while (current->next != nullptr) {
+            prev = current;
+            current = current->next;
+        }
+
+        delete current;
+        prev->next = nullptr;
     }
-
-
-
-
 };
